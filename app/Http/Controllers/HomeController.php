ac909fa94cc5acbdf2027aa9e56670491284a7a6
Hieu Vo <hieuvtm3010@gmail.com>
2021-11-23 22:50:24 +0700

    [update] filter product

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,41 +4,51 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Resources\HomeResource;
-use App\Product, App\Attribute;
+use App\Product, App\Attribute, App\Brand;
 use WithPagination;
 class HomeController extends Controller
 {
     //
     public function show_product(Request $request){ 
         $this->pagesize = 10;
+        $s = Product::with('brand');
+     
+        
         if (isset($_GET['sort_by'])) {
             $sort_by = $_GET['sort_by'];
-            if($sort_by == 'latested'){
-                $products = Product::orderBy('created_at','DESC')->paginate($this->pagesize);
-            }elseif($sort_by == 'old'){
-                $products = Product::orderBy('created_at','asc')->paginate($this->pagesize);
-            }elseif($sort_by == 'price-desc'){
-                $products = Product::orderBy('price','DESC')->paginate($this->pagesize);
-            }elseif($sort_by == 'price-asc'){
-                $products = Product::orderBy('price','ASC')->paginate($this->pagesize);
-            }elseif($sort_by == 'price-discount'){
-                $products = Product::orderBy('discount','DESC')->paginate($this->pagesize);
+            if($sort_by == 'discount'){
+                $s->orderBy('discount','DESC');
+            }elseif($sort_by == 'desc'){
+                $s->orderBy('price','DESC');
+            }elseif($sort_by == 'asc'){
+                $s->orderBy('price','ASC');
             }
         }
+        
+
+
         if(isset($_GET['price_start']) && isset($_GET['price_end'])) {
             $price_start = $_GET['price_start'];
             $price_end = $_GET['price_end'];
-            $products = Product::where('status', '1')->whereBetween('price',[$price_start,$price_end])->paginate($this->pagesize);
+            $s->whereBetween('price',[$price_start,$price_end])->paginate($this->pagesize);
             
         }
-        if(isset($_GET['id'])){
-            $sort_by = $_GET['id'];
-            $products = Product::where('status', '1')->with('brand')->where('brand_id', $id)->paginate($this->pagesize);
+        if(isset($_GET['brand_id'])){
+            if(isset($_GET['brand_id1']) && isset($_GET['brand_id2'])){
+                $s->whereIn('brand_id', [$_GET['brand_id'],$_GET['brand_id1'],$_GET['brand_id2']]);
+            }elseif(isset($_GET['brand_id2'])){
+                $s->whereIn('brand_id', [$_GET['brand_id'],$_GET['brand_id2']]);
+            }elseif(isset($_GET['brand_id1'])){
+                $s->whereIn('brand_id', [$_GET['brand_id'],$_GET['brand_id1']]);
+            }else{
+                $s->where('brand_id', [$_GET['brand_id']]);
+            }
+            //$products = Product::where('status', '1')->with('brand')->where('brand_id', $id)->paginate($this->pagesize);
         }
         return response([
             'message' => 'Success products sort',
             'data' => 
-               HomeResource::collection($products),
+               HomeResource::collection($s->get()),
         ], 201);
     }
 
